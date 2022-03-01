15f72ccf94a2436ac76e1192640b607dcc3b272f
msnpy <53246704+msnpy@users.noreply.github.com>
2022-02-28 23:20:56 -0300

    atualizado

---
+++ init/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    	let elems = document.querySelectorAll('.sidenav');
+    	let instances = M.Sidenav.init(elems,);
+
+  });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let  elems = document.querySelectorAll('.carousel');
+    let instances = M.Carousel.init(elems,{ indicators: true});
+  });
+
