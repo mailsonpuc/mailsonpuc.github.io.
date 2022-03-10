a0a2864c041137a31163d596f5801a004b9868e6
M4isv <53246704+m4isv@users.noreply.github.com>
2022-03-10 13:45:25 -0300

    Update hour.js
    no pm or am

--- init/js/hour.js
+++ init/js/hour.js
@@ -1,6 +1,8 @@
 const myInterval = setInterval(myTimer, 1000)
 function myTimer() {
   const date = new Date();
-  document.getElementById("txt0").innerHTML = `<b>${date.toLocaleTimeString()}</b>`
+  document.getElementById("txt0").innerHTML = `<b>${date.toLocaleTimeString().split(' ')[0]
+}</b>`
 }
+
 
